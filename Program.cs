e392e58af61f65ed5c58da0b3878f13346bfff6d
thuynga23 <thuyngatrinh23@gmail.com>
2024-09-18 08:33:40 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,150 +1,118 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 
-// Lop cha: San pham
-class Product
+class BaiTap
 {
-    public string TenSanPham { get; set; }
-    public decimal Gia { get; set; }
-    public string MoTa { get; set; }
-    public int SoLuong { get; set; }
-
-    public Product(string tenSanPham, decimal gia, string moTa, int soLuong)
+    // Bai 1: Tao mang so nguyen va tinh tong
+    public void Bai1()
     {
-        TenSanPham = tenSanPham;
-        Gia = gia;
-        MoTa = moTa;
-        SoLuong = soLuong;
-    }
+        Console.WriteLine("Bai 1: Tinh tong cac phan tu trong mang");
+        Console.Write("Nhap so luong phan tu trong mang: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] mang = new int[n];
 
-    public virtual void HienThiThongTin()
-    {
-        Console.WriteLine($"San pham: {TenSanPham}, Gia: {Gia}, Mo ta: {MoTa}, So luong: {SoLuong}");
-    }
-}
-
-// Lop con: Dien tu
-class Electronic : Product
-{
-    public int BaoHanh { get; set; } // Thang bao hanh
-
-    public Electronic(string tenSanPham, decimal gia, string moTa, int soLuong, int baoHanh)
-        : base(tenSanPham, gia, moTa, soLuong)
-    {
-        BaoHanh = baoHanh;
-    }
-
-    public override void HienThiThongTin()
-    {
-        base.HienThiThongTin();
-        Console.WriteLine($"Bao hanh: {BaoHanh} thang");
-    }
-}
-
-// Lop con: Quan ao
-class Clothing : Product
-{
-    public string KichThuoc { get; set; }
-    public string MauSac { get; set; }
-
-    public Clothing(string tenSanPham, decimal gia, string moTa, int soLuong, string kichThuoc, string mauSac)
-        : base(tenSanPham, gia, moTa, soLuong)
-    {
-        KichThuoc = kichThuoc;
-        MauSac = mauSac;
-    }
-
-    public override void HienThiThongTin()
-    {
-        base.HienThiThongTin();
-        Console.WriteLine($"Kich thuoc: {KichThuoc}, Mau sac: {MauSac}");
-    }
-}
-
-// Lop con: Thuc pham
-class Food : Product
-{
-    public DateTime NgayHetHan { get; set; }
-
-    public Food(string tenSanPham, decimal gia, string moTa, int soLuong, DateTime ngayHetHan)
-        : base(tenSanPham, gia, moTa, soLuong)
-    {
-        NgayHetHan = ngayHetHan;
-    }
-
-    public override void HienThiThongTin()
-    {
-        base.HienThiThongTin();
-        Console.WriteLine($"Ngay het han: {NgayHetHan.ToShortDateString()}");
-    }
-}
-
-// Lop Gio hang
-class ShoppingCart
-{
-    private List<Product> danhSachSanPham;
-
-    public ShoppingCart()
-    {
-        danhSachSanPham = new List<Product>();
-    }
-
-    public void ThemSanPham(Product sanPham)
-    {
-        danhSachSanPham.Add(sanPham);
-        Console.WriteLine($"Da them san pham: {sanPham.TenSanPham}");
-    }
-
-    public void XoaSanPham(Product sanPham)
-    {
-        danhSachSanPham.Remove(sanPham);
-        Console.WriteLine($"Da xoa san pham: {sanPham.TenSanPham}");
-    }
-
-    public void HienThiGioHang()
-    {
-        Console.WriteLine("Cac san pham trong gio hang:");
-        foreach (var sanPham in danhSachSanPham)
+        for (int i = 0; i < n; i++)
         {
-            sanPham.HienThiThongTin();
+            Console.Write($"Nhap phan tu thu {i + 1}: ");
+            mang[i] = int.Parse(Console.ReadLine());
         }
+
+        int tong = mang.Sum();
+        Console.WriteLine($"Tong cac phan tu trong mang la: {tong}");
     }
 
-    public decimal TinhTongGiaTri()
+    // Bai 2: Dem so luong ky tu trong chuoi
+    public void Bai2()
     {
-        decimal tongGia = 0;
-        foreach (var sanPham in danhSachSanPham)
-        {
-            tongGia += sanPham.Gia * sanPham.SoLuong;
-        }
-        return tongGia;
+        Console.WriteLine("\nBai 2: Dem so luong ky tu trong chuoi");
+        Console.Write("Nhap chuoi: ");
+        string chuoi = Console.ReadLine();
+        int dem = chuoi.Count(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c));
+        Console.WriteLine($"So luong ky tu khong tinh khoang trang va dau cau la: {dem}");
     }
-}
 
-class Program
-{
+    // Bai 3: Tim phan tu lon nhat trong mang
+    public void Bai3()
+    {
+        Console.WriteLine("\nBai 3: Tim phan tu lon nhat trong mang");
+        Console.Write("Nhap so luong phan tu trong mang: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] mang = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu thu {i + 1}: ");
+            mang[i] = int.Parse(Console.ReadLine());
+        }
+
+        int max = mang.Max();
+        Console.WriteLine($"Phan tu lon nhat trong mang la: {max}");
+    }
+
+    // Bai 4: Dao nguoc chuoi
+    public void Bai4()
+    {
+        Console.WriteLine("\nBai 4: Dao nguoc chuoi");
+        Console.Write("Nhap chuoi: ");
+        string chuoi = Console.ReadLine();
+        string daoNguoc = new string(chuoi.Reverse().ToArray());
+        Console.WriteLine($"Chuoi sau khi dao nguoc: {daoNguoc}");
+    }
+
+    // Bai 5: Kiem tra mang doi xung
+    public void Bai5()
+    {
+        Console.WriteLine("\nBai 5: Kiem tra mang doi xung");
+        Console.Write("Nhap so luong phan tu trong mang: ");
+        int n = int.Parse(Console.ReadLine());
+        int[] mang = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write($"Nhap phan tu thu {i + 1}: ");
+            mang[i] = int.Parse(Console.ReadLine());
+        }
+
+        bool doiXung = true;
+        for (int i = 0; i < n / 2; i++)
+        {
+            if (mang[i] != mang[n - 1 - i])
+            {
+                doiXung = false;
+                break;
+            }
+        }
+
+        if (doiXung)
+            Console.WriteLine("Mang la doi xung.");
+        else
+            Console.WriteLine("Mang khong phai la doi xung.");
+    }
+
+    // Bai 6: Dem so lan xuat hien cua mot ky tu trong chuoi
+    public void Bai6()
+    {
+        Console.WriteLine("\nBai 6: Dem so lan xuat hien cua mot ky tu trong chuoi");
+        Console.Write("Nhap chuoi: ");
+        string chuoi = Console.ReadLine();
+        Console.Write("Nhap ky tu can dem: ");
+        char kyTu = char.Parse(Console.ReadLine());
+
+        int dem = chuoi.Count(c => c == kyTu);
+        Console.WriteLine($"So lan xuat hien cua ky tu '{kyTu}' trong chuoi la: {dem}");
+    }
+
+    // Phuong thuc Main de goi cac bai tap
     static void Main(string[] args)
     {
-        // Tao cac san pham mau
-        var dienThoai = new Electronic("Dien thoai", 5000000, "Dien thoai thong minh", 2, 12);
-        var aoThun = new Clothing("Ao thun", 150000, "Ao thun cotton", 3, "L", "Do");
-        var banhMi = new Food("Banh mi", 20000, "Banh mi tuoi", 10, DateTime.Now.AddDays(5));
+        BaiTap baiTap = new BaiTap();
 
-        // Tao gio hang
-        var gioHang = new ShoppingCart();
-
-        // Them san pham vao gio hang
-        gioHang.ThemSanPham(dienThoai);
-        gioHang.ThemSanPham(aoThun);
-        gioHang.ThemSanPham(banhMi);
-
-        // Hien thi cac san pham trong gio hang
-        gioHang.HienThiGioHang();
-
-        // Tinh tong gia tri don hang
-        decimal tongGiaTri = gioHang.TinhTongGiaTri();
-        Console.WriteLine($"Tong gia tri don hang: {tongGiaTri} VND");
-
-        Console.ReadLine();
+        // Goi cac phuong thuc de chay tung bai
+        baiTap.Bai1();
+        baiTap.Bai2();
+        baiTap.Bai3();
+        baiTap.Bai4();
+        baiTap.Bai5();
+        baiTap.Bai6();
     }
 }
