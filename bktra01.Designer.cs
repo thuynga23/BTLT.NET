88fa88826dd50c1a1396577f0641a0f3b73b0084
thuynga23 <thuyngatrinh23@gmail.com>
2024-10-30 09:59:02 +0700

    Add files via upload

---
+++ bktra01.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace bktra01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvProducts = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.btnAddToCart = new System.Windows.Forms.Button();
+            this.btnRemoveFromCart = new System.Windows.Forms.Button();
+            this.btnCheckout = new System.Windows.Forms.Button();
+            this.dgvCart = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lblTotalQuantity = new System.Windows.Forms.Label();
+            this.lblTotalPrice = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lvProducts
+            // 
+            this.lvProducts.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvProducts.HideSelection = false;
+            this.lvProducts.Location = new System.Drawing.Point(12, 13);
+            this.lvProducts.Name = "lvProducts";
+            this.lvProducts.Size = new System.Drawing.Size(279, 333);
+            this.lvProducts.TabIndex = 0;
+            this.lvProducts.UseCompatibleStateImageBehavior = false;
+            this.lvProducts.View = System.Windows.Forms.View.Details;
+            this.lvProducts.SelectedIndexChanged += new System.EventHandler(this.lvProducts_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Tên sản phẩm";
+            this.columnHeader1.Width = 109;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Giá";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Số lượng";
+            this.columnHeader3.Width = 95;
+            // 
+            // btnAddToCart
+            // 
+            this.btnAddToCart.Location = new System.Drawing.Point(12, 352);
+            this.btnAddToCart.Name = "btnAddToCart";
+            this.btnAddToCart.Size = new System.Drawing.Size(143, 34);
+            this.btnAddToCart.TabIndex = 1;
+            this.btnAddToCart.Text = "Thêm vào giỏ hàng";
+            this.btnAddToCart.UseVisualStyleBackColor = true;
+            this.btnAddToCart.Click += new System.EventHandler(this.btnAddToCart_Click);
+            // 
+            // btnRemoveFromCart
+            // 
+            this.btnRemoveFromCart.Location = new System.Drawing.Point(161, 352);
+            this.btnRemoveFromCart.Name = "btnRemoveFromCart";
+            this.btnRemoveFromCart.Size = new System.Drawing.Size(143, 34);
+            this.btnRemoveFromCart.TabIndex = 2;
+            this.btnRemoveFromCart.Text = "Xóa khỏi giỏ hàng";
+            this.btnRemoveFromCart.UseVisualStyleBackColor = true;
+            this.btnRemoveFromCart.Click += new System.EventHandler(this.btnRemoveFromCart_Click);
+            // 
+            // btnCheckout
+            // 
+            this.btnCheckout.Location = new System.Drawing.Point(84, 392);
+            this.btnCheckout.Name = "btnCheckout";
+            this.btnCheckout.Size = new System.Drawing.Size(143, 34);
+            this.btnCheckout.TabIndex = 3;
+            this.btnCheckout.Text = "Thanh toán";
+            this.btnCheckout.UseVisualStyleBackColor = true;
+            this.btnCheckout.Click += new System.EventHandler(this.btnCheckout_Click);
+            // 
+            // dgvCart
+            // 
+            this.dgvCart.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvCart.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dgvCart.Location = new System.Drawing.Point(310, 13);
+            this.dgvCart.Name = "dgvCart";
+            this.dgvCart.ReadOnly = true;
+            this.dgvCart.RowHeadersWidth = 51;
+            this.dgvCart.RowTemplate.Height = 24;
+            this.dgvCart.Size = new System.Drawing.Size(665, 343);
+            this.dgvCart.TabIndex = 4;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Tên sản phẩm";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 125;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Giá";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 125;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Số lượng";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 125;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Thanh toán";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 125;
+            // 
+            // lblTotalQuantity
+            // 
+            this.lblTotalQuantity.AutoSize = true;
+            this.lblTotalQuantity.Location = new System.Drawing.Point(343, 370);
+            this.lblTotalQuantity.Name = "lblTotalQuantity";
+            this.lblTotalQuantity.Size = new System.Drawing.Size(106, 16);
+            this.lblTotalQuantity.TabIndex = 5;
+            this.lblTotalQuantity.Text = "Tổng số lượng: 0";
+            // 
+            // lblTotalPrice
+            // 
+            this.lblTotalPrice.AutoSize = true;
+            this.lblTotalPrice.Location = new System.Drawing.Point(346, 410);
+            this.lblTotalPrice.Name = "lblTotalPrice";
+            this.lblTotalPrice.Size = new System.Drawing.Size(87, 16);
+            this.lblTotalPrice.TabIndex = 6;
+            this.lblTotalPrice.Text = "Tổng giá trị: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(987, 450);
+            this.Controls.Add(this.lblTotalPrice);
+            this.Controls.Add(this.lblTotalQuantity);
+            this.Controls.Add(this.dgvCart);
+            this.Controls.Add(this.btnCheckout);
+            this.Controls.Add(this.btnRemoveFromCart);
+            this.Controls.Add(this.btnAddToCart);
+            this.Controls.Add(this.lvProducts);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvCart)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvProducts;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.Button btnAddToCart;
+        private System.Windows.Forms.Button btnRemoveFromCart;
+        private System.Windows.Forms.Button btnCheckout;
+        private System.Windows.Forms.DataGridView dgvCart;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.Label lblTotalQuantity;
+        private System.Windows.Forms.Label lblTotalPrice;
+    }
+}
+
---
+++ bktra01.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace bktra01
+{
+    public partial class Form1 : Form
+    {
+        public class Product
+        {
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+
+            public Product(string name, decimal price, int quantity = 1)
+            {
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Cấu hình ListView
+            lvProducts.View = View.Details;
+            lvProducts.Columns.Add("Tên sản phẩm", 150);
+            lvProducts.Columns.Add("Giá", 100);
+            lvProducts.Columns.Add("Số lượng", 100);
+
+            // Thêm sản phẩm mẫu vào ListView
+            lvProducts.Items.Add(new ListViewItem(new[] { "bánh tyty", "10000", "1" }));
+            lvProducts.Items.Add(new ListViewItem(new[] { "sữa vinamilk", "20000", "1" }));
+            lvProducts.Items.Add(new ListViewItem(new[] { "sữa TH", "15000", "1" }));
+            // Cấu hình DataGridView
+            dgvCart.ColumnCount = 4;
+            dgvCart.Columns[0].Name = "Tên sản phẩm";
+            dgvCart.Columns[1].Name = "Giá";
+            dgvCart.Columns[2].Name = "Số lượng";
+            dgvCart.Columns[3].Name = "Thanh toán";
+            dgvCart.Columns[3].ReadOnly = true; // Không cho phép chỉnh sửa cột Thanh toán
+        }
+
+        private void lvProducts_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddToCart_Click(object sender, EventArgs e)
+        {
+            if (lvProducts.SelectedItems.Count > 0)
+            {
+                // Lấy thông tin sản phẩm từ ListView
+                ListViewItem selectedItem = lvProducts.SelectedItems[0];
+                string productName = selectedItem.SubItems[0].Text;
+                decimal price = decimal.Parse(selectedItem.SubItems[1].Text);
+                int quantity = int.Parse(selectedItem.SubItems[2].Text);
+
+                // Tính tổng giá trị
+                decimal total = price * quantity;
+
+                // Thêm vào DataGridView
+                dgvCart.Rows.Add(productName, price, quantity, total);
+
+                // Cập nhật tổng số lượng và tổng giá trị
+                UpdateCartSummary();
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng chọn một sản phẩm từ danh sách.");
+            }
+        }
+
+        private void btnRemoveFromCart_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in dgvCart.SelectedRows)
+            {
+                dgvCart.Rows.Remove(row);
+            }
+            UpdateCartSummary();
+        }
+        private void UpdateCartSummary()
+        {
+            int totalQuantity = 0;
+            decimal totalPrice = 0;
+
+            foreach (DataGridViewRow row in dgvCart.Rows)
+            {
+                totalQuantity += Convert.ToInt32(row.Cells["Số lượng"].Value);
+                totalPrice += Convert.ToDecimal(row.Cells["Thanh toán"].Value);
+            }
+
+            lblTotalQuantity.Text = "Tổng số lượng: " + totalQuantity;
+            lblTotalPrice.Text = "Tổng giá trị: " + totalPrice;
+        }
+
+        private void btnCheckout_Click(object sender, EventArgs e)
+        {
+            if (dgvCart.Rows.Count > 0)
+            {
+                MessageBox.Show("Thanh toán thành công! Tổng giá trị: " + lblTotalPrice.Text);
+                dgvCart.Rows.Clear(); // Xóa toàn bộ sản phẩm trong giỏ hàng
+                UpdateCartSummary();   // Cập nhật lại tổng số lượng và tổng giá trị
+            }
+            else
+            {
+                MessageBox.Show("Giỏ hàng của bạn đang trống.");
+            }
+        }
+    }
+}
